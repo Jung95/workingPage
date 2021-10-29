d1dbc3fb8ff28255c0cd7001c31a7512a5be1fbc
Jung95 <33000117+Jung95@users.noreply.github.com>
2021-10-29 21:17:45 +0900

    set time text with npm --time=

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+require("dotenv").config();
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { time: process.env.npm_config_time });
 });
 
 module.exports = router;
